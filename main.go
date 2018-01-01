9964ea5855bbb5fc972b617a6695d01d2a98db3c
ardeshir sepahsalar <ardeshir.org@gmail.com>
2018-01-01 20:21:38 +0000

    adding time & version

--- main.go
+++ main.go
@@ -1,45 +1,15 @@
 package main
-import "fmt"
+
+// go build -ldflags "-X main.version=0.0.1"
+
+import (
+   v "github.com/ardeshir/version"
+)
+
+var version string
+
 func main() {
 
-   x := []int{6,7,8,9,5}
-
-    fmt.Println(len(x))
+  v.V(version)
    
-   /* x[0] = 5
-   x[1] = 6
-   x[2] = 7
-   x[3] = 8
-   x[4] = 9
-   */
-   
-     // for i := 0; i < len(x) ; i++  {
-     for i, v := range x {
-        
-      fmt.Printf("%d\t%d\t = %v\n", i, v, x[i])
-    
-    }
-    
-    fmt.Println("Before the append() \n")
-    fmt.Println(x)
-    // slice the slice
-    fmt.Println(x[1:len(x)])
-    
-    x = append(x, 87, 80, 90, 100, 182)
-    
-    
-     fmt.Println("After append()\n")
-     fmt.Println(x)
-    // slice the slice
-    fmt.Println(x[1:len(x)])
-    
-    y := []int{1212, 12312, 346, 66}
-    x = append(x, y...)
-    
-    fmt.Println("After append(x, y...)\n")
-     fmt.Println(x)
-    // slice the slice
-    fmt.Println(x[1:len(x)])
-    
-    
 }
